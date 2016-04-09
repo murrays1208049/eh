ed27c5a7cad91acbb530cc366ce99eeec15b99c5
murrays1208049 <ml2257875@163.com>
2016-04-10 04:01:37 +0800

    eh-1 // Update product data type

--- src/com/eh/api/Product.java
+++ src/com/eh/api/Product.java
@@ -3,6 +3,30 @@ package com.eh.api;
 public class Product
 {
 	private int id = 0;
+	
+	private String name="";
+	
+	public String getUtil()
+	{
+		return this.util;
+	}
+
+	public void setUtil(String util)
+	{
+		this.util = util;
+	}
+
+	private String util="";
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
 
 	public int getId()
 	{
